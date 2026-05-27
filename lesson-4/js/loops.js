35deb1353df34d74853a9d41c895ce8f52319c3c
Shy-Anna Lee <shyjah89@gmail.com>
2026-05-27 10:49:10 -0400

    loops complete

--- lesson-4/js/loops.js
+++ lesson-4/js/loops.js
@@ -19,19 +19,25 @@ msg.textContent = courseList;
 let cities = ['Toronto', 'Montreal', 'Ottawa', 'Vancouver', 'Halifax'];
 const result = document.querySelector('#searchResult');
 const input = document.querySelector('input');
-const btn = document.querySelector('button#cities');
+const btn = document.querySelector('#cities');
 
 btn.addEventListener('click', function() {
-	var searchName = input.value;
+	let searchName = input.value;
 	input.value = '';
 	input.focus();
 	/* STEP 2a: Create a FOR loop to iterate through the cities array */
-
+	for (let i = 0; i < cities.length; i ++) {
 		/* STEP 2b: Build an IF/ELSE conditional that compares searchName with cities[i] */
-
+		if (searchName.trim().toLowerCase() === cities[i].toLowerCase()) {
+			// STEP 2c: We found the city!
+			result.textContent = `Found ${cities[i]}!`;
+			break;
+		} else {
+			result.textContent = `Sorry, ${searchName} not found.`;
+		}
+		console.log(cities[i]);
+	}
 });
-
-
 
 /* STEP 3: Skip a loop interation with continue */
 const numberList = document.querySelector('#numberList');
@@ -39,10 +45,10 @@ let num = 50;
 for (let i = 1; i <= num; i ++) {
 	let sqRoot = Math.sqrt(i);
 	/* STEP 3a: Build an IF statement that checks whether the square root of the number is NOT an integer */
-	//if () {
+	if (Math.floor(sqRoot) !== sqRoot) {
 		/* STEP 3b: If the square root of the number is not an integer, we don't want to output it to the paragraph, so skip the rest of the instructions inside this loop and go back up to the FOR, using 'continue' */
-
-	//}
+		continue;
+	}
 	numberList.textContent += i + ' ';
 }
 
@@ -51,18 +57,26 @@ for (let i = 1; i <= num; i ++) {
 let j = 0;
 let outputWhile = '';
 /* STEP 4a: WHILE loop to iterate through the cities array (created above in STEP 2) */
+while (j < cities.length) {
+	outputWhile += cities[j] + " / ";
+	j ++;
+}
+// console.log(outputWhile);
 
-console.log(outputWhile);
-
-let k = 0;
+let k = cities.length - 1;
 var outputDo = '';
 /* STEP 4b: DO/WHILE loop - grab the above IF/ELSE and the 'i++', then create the very same output with DO/WHILE (uncomment the above 'i = 0', first) */
-
+do {
+	outputDo += cities[k] + " / ";
+	k --;
+} while (k >= 0);
 console.log(outputDo);
 /* …now try to loop through the same array backwards! */
 
 /* STEP 5: The elegant FOREACH loop (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach) */
-
+cities.forEach((city) => {
+	console.warn(city);
+})
 
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Looping_code
