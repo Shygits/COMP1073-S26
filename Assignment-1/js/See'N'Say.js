736d621fdcff96f1988a055509d951470bea3201
Shy-Anna Lee <shyjah89@gmail.com>
2026-06-13 22:52:11 -0400

    arrays and indexes roughed in

---
+++ Assignment-1/js/See'N'Say.js
@@ -0,0 +1,14 @@
+//Story Arrays for each column
+const part1 = ["A", "B", "C", "D", "E"];
+const part1 = ["A", "B", "C", "D", "E"];
+const part1 = ["A", "B", "C", "D", "E"];
+const part1 = ["A", "B", "C", "D", "E"];
+const part1 = ["A", "B", "C", "D", "E"];
+
+//Index variables track selection
+let index1 = 0;
+let index1 = 0;
+let index1 = 0;
+let index1 = 0;
+let index1 = 0;
+
