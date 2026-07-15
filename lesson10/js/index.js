81f7c913fd89e3f30040e71db652488ecee50891
Shy-Anna Lee <shyjah89@gmail.com>
2026-07-15 19:53:11 -0400

    compete

--- lesson10/js/index.js
+++ lesson10/js/index.js
@@ -1,70 +1,62 @@
-const output = document.querySelector('#output');
+const output = document.querySelector("#output");
 
-/* STEP 1: Create an object (looks a lot like declaring a variable, but with empty braces), then open this page in a browser and enter 'Coffee' in the console */
+/* STEP 1: Create an object (looks a lot like declaring a variable, but with empty braces), 
+then open this page in a browser and enter 'Coffee' in the console */
 function Coffee(size, isDecaf, qtyCream, qtySugar) {
     this.size = size;
-    this.Decaf = isDecaf;
+    this.isDecaf = isDecaf;
     this.qtyCream = qtyCream;
     this.qtySugar = qtySugar;
-    this.decaf = this.isDecaf ? "decaffinated" : "caffeinated";
-    this.description = function() {
-        //A Small caffeinated coffee with 1 cream and 2 sugar
-        return `A ${this.size} ${this.decaf} coffee with ${this.qtyCream} cream and ${this.qtySugar} sugar`;
-
-    }
-
+    this.decaf = this.isDecaf ? "decaffinated" : "caffinated";
+    this.description = function () {
+        // A small caffinated coffee with 1 cream and 2 sugar
+        return `A ${this.size} ${this.decaf} coffee with ${this.qtyCream} cream and ${this.qtySugar} sugar.`;
+    };
 }
+
 /* STEP 2: Instatiate a coffee based on the above constructor function */
-let shyCoffee = new Coffee('Large', false, 3, 3);
-output.textContent = shyCoffee.description();
+let priyanshCoffee = new Coffee("medium", false, 2, 1);
+output.textContent = priyanshCoffee.description();
 
-/* STEP 3: Refresh the page, and in the console, begin to call a method on shyCoffee by typing 'shyCoffee.' - look at all the members and methods */
+/* STEP 3: Refresh the page, and in the console, begin to call a method on priyanshCoffee by typing 'priyanshCoffee.' - look at all the members and methods */
 
+/* STEP 4: Enter into the console, priyanshCoffee.valueOf() and look at the result. 
+priyanshCoffee doesn't have such a method, and neither does the constructor function, 'Coffee'. But the 'Object' object does - so through inheritance, priyanshCoffee has access to the method, valueOf(). */
 
-/* STEP 4: Enter into the console, shyCoffee.valueOf() and look at the result. shyCoffee doesn't have such a method, and neither does the constructor function, 'Coffee'. But the 'Object' object does - so through inheritance, shyCoffee has access to the method, valueOf(). */
+/* STEP 5a: Each object has a prototype member that isn't really directly accessible, 
+but it can be referenced using the deprecated '__proto__' - using a double underscore on either side of the word 'proto'. 
+Try it - priyanshCoffee.__proto__.__proto__ */
 
-
-/* STEP 5a: Each object has a prototype member that isn't really directly accessible, but it can be referenced using the deprecated '__proto__' - using a double underscore on either side of the word 'proto'. Try it - shyCoffee.__proto__.__proto__ */
-
-
-/* STEP 5b: The more modern way to do this is … Object.getPrototypeOf(shyCoffee) - try THIS in the console */
-
+/* STEP 5b: The more modern way to do this is … Object.getPrototypeOf(priyanshCoffee) - try THIS in the console */
 
 /* STEP 5c: EVERYTHING is an object in JavaScript. Try accessing the prototype property of Coffee (which even though it is a constructor function it is still an object) with Coffee.prototype in the console. Then try Object.Prototype */
 
+/* STEP 6a: Let's circle back to create() - use priyanshCoffee to create a new object instance - one based on priyanshCoffee. */
+let robertCoffee = Object.create(priyanshCoffee);
+robertCoffee.size = "large";
+robertCoffee.isDecaf = true;
+robertCoffee.qtyCream = 1;
+robertCoffee.qtySugar = 1;
 
-/* STEP 6a: Let's circle back to create() - use scottsCoffee to create a new object instance - one based on shyCoffee. */
-let stefCoffee = Object.create(shyCoffee);
-stefCoffe.size = "Medium";
-stefCoffee.isDecaf = true;
-stefCoffee.qtyCream = 1;
-stefCoffee.qtySugar =3;
+/* STEP 6b: See how this new object inherits from the prototype with robertCoffee.__proto__ in the console. */
 
-
-/* STEP 6b: See how this new object inherits from the prototype with stefCoffee.__proto__ in the console. */
-
-
-/* STEP 7a: Each constructor function includes a prototype property with a value equal to an object that contains a constructor property. Try it out by typing shyCoffee.constructor and stefCoffee.constructor */
-
+/* STEP 7a: Each constructor function includes a prototype property with a value equal to an object that contains a constructor 
+property. Try it out by typing priyanshCoffee.constructor and robertCoffee.constructor */
 
 /* STEP 7b: Since constructor is also a function, you can use it to create a new object instance - try it! */
-let eliCoffee = new stefCoffee.constructor("small", true, 4, 3);
+let meganCoffee = new robertCoffee.constructor("small", false, 2, 3);
 
-/* STEP 7c: Attempt via the console to access the new object's properties - eliCoffee.size, eliCoffee.isDecaf, etc. */
-
+/* STEP 7c: Attempt via the console to access the new object's properties - meganCoffee.size, meganCoffee.isDecaf, etc. */
 
 /* STEP 7d: Now see if the new object can access the description() method… */
 
-
-/* STEP 7e: The constructor has other features - try using it to discover the name of an instance's constructor by typing eliCoffee.constructor.name into the console. */
-
+/* STEP 7e: The constructor has other features - try using it to discover the name of an instance's constructor by typing meganCoffee.constructor.name into the console. */
 
 /* STEP 8a: We can modify the prototype property of a constructor function - let's add another method to Coffee */
 
-
 // What's really interesting about the above code is that every instance based on the Coffee constructor has been updated due to prototypical inheritance!
 
-/* STEP 8b: Now let's call up this method on one or more of our Coffee instances from the console (scottsCoffee, richsCoffee, or kathysCoffee). */
+/* STEP 8b: Now let's call up this method on one or more of our Coffee instances from the console (priyanshCoffee, robertCoffee, or meganCoffee). */
 
 // Next, open up coffee.html and we will see how to leverage object inheritance with JavaScript classes.
 
