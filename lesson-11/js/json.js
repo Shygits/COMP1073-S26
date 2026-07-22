f4be418bea0b3220d5f9487264529a584e13259c
Shy-Anna Lee <shyjah89@gmail.com>
2026-07-22 19:22:14 -0400

    step 1-9a complete

--- lesson-11/js/json.js
+++ lesson-11/js/json.js
@@ -3,24 +3,37 @@ const header = document.querySelector("header");
 const section = document.querySelector("section");
 
 // STEP 3a: Create the asynchronous function populate()
+async function populate() {
+
 
 // Introducing JavaScript Object Notation (JSON): https://json.org/
 // STEP 4a: Create i-scream.json file with companyName, headOffice, established, active, topFlavors(name, calories, type, ingredients, image) */
 // STEP 4b: Store the URL of a JSON file in a variable */
+const url = "https://shygits.github.io/COMP1073-S26/lesson-11/js/shy-scream.json";
 
 // STEP 5: Use the new URL to create a new request object
+const request = new Request(url);
+
 
 // STEP 6: Make a network request with the fetch() function, which returns a Response object
+const response = await fetch(request);
+console.log(request);
 
 // STEP 7: Capture the returned Response object and covert to a JSON object using json()
+const responseJson = await response.json();
 
 // STEP 8: Output the iScream JSON object to the console
+console.log(responseJson);
 
 // STEP 9a: Invoke the populateHeader function here, then build it below
+populateHeader(responseJson);
 
 // STEP 10a: Invoke the showTopFlavors function here, then build it below
+showTopFlavors(responseJson);
+}
 
 // STEP 3b: Call the populate() function
+populate();
 
 /* STEP 9b: Build out the populateHeader() function */
 
