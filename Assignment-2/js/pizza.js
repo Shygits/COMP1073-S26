badb8dc837699702ac8cd315df847a710911b7d9
Shy-Anna Lee <shyjah89@gmail.com>
2026-07-26 22:44:07 -0400

    minor changes to css and js

--- Assignment-2/js/pizza.js
+++ Assignment-2/js/pizza.js
@@ -2,3 +2,25 @@
 const studentInfo = document.querySelector("#studentInfo");
 studentInfo.textContent = "Shy-Anna Lee |  Student ID: 200640675";
 
+
+//Form handling + validation
+const pizzaForm = document.querySelector("#pizzaForm");
+const output = document.querySelector("#output");
+
+pizzaForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // Capture Form Values
+    const customerName = document.querySelector("#customerName").value.trim();
+    
+    const size = document.querySelector("input[name='size']:checked");
+    
+    const crust = document.querySelector("#crust").value;
+    
+    const sauce = document.querySelector("#sauce").value;
+
+    //TOPPINGS
+    
+    
+
+});
