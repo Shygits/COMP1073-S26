6f5d07548654eafa6beaacd261609dd1ae929a5e
Shy-Anna Lee <shyjah89@gmail.com>
2026-08-07 15:03:05 -0400

    Adding display title etc

--- Assignment-3/js/metArt.js
+++ Assignment-3/js/metArt.js
@@ -17,3 +17,41 @@ fetch(url)
         });
 }
 
+//Display art in a card
+function displayArtwork(data) {
+
+    //creates card container
+    let card = document.createElement("div");
+    card.classList.add("art-card");
+
+    //creates image element
+    let img =  document.createElement("img");
+    img.src = data.primaryImage;
+    img.alt = data.title;
+
+    //TITLE
+    let title = document.createElement("p");
+    title.classList.add("art-title");
+    title.textContent = data.title;
+
+    // ARTIST
+    let artist = document.createElement("p");
+    artist.classList.add("art-artist");
+    artist.textContent = "Artist: " + data.artistDisplayName;
+
+    //YEAR
+    let year = document.createElement("p");
+    year.classList.add("art-year");
+    year.textContent = "Year: " + data.objectDate;
+    
+    // ADD ELEMENTS TO CARD
+    card.appendChild(img);
+    card.appendChild(title);
+    card.appendChild(artist);
+    card.appendChild(year);
+
+    // Add card to page
+    artContainer.appendChild(card);
+}
+
+// Fetch and display artworks for the given object IDs
